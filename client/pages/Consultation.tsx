6f73d4efdc3a7d7d0c2ca5d8985517cf5dec5b52
Yathiraj335 <Yathirajputtaswamy@gmail.com>
2026-05-04 10:24:02 +0530

    Update redirect URL in Consultation component

--- client/pages/Consultation.tsx
+++ client/pages/Consultation.tsx
@@ -53,7 +53,7 @@ export default function Consultation() {
               <input
                 type="hidden"
                 name="redirect"
-                value="/thank-you"
+                value="https://rameshlawassociates.netlify.app/thank-you" />
               />
 
               {/* Name */}
