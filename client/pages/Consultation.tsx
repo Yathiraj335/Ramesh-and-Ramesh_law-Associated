34cea35ab9e92ee1c74fdacb62e31de652cf51a1
kunaase <85295079+kunaase@users.noreply.github.com>
2026-05-06 14:52:46 +0530

    Update Consultation.tsx

--- client/pages/Consultation.tsx
+++ client/pages/Consultation.tsx
@@ -53,7 +53,7 @@ export default function Consultation() {
               <input
                 type="hidden"
                 name="redirect"
-                value="https://rameshlawassociates.netlify.app/thankyou"
+                value="https://rameshlawassociates.com/thankyou"
               />
 
               {/* Name */}
